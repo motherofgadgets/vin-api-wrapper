e0b5859e7cefe85e4402f4cc9702088b05feba43
alanna <alanna.june.moss@gmail.com>
2023-05-25 19:50:19 -0400

    Added test database for unit testing

--- vindecode/tests/test_vindecode.py
+++ vindecode/tests/test_vindecode.py
@@ -1,8 +1,35 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from vindecode.main import app
+from vindecode.database import Base
+from vindecode.main import app, get_db
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
