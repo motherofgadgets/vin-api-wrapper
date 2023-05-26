9dd6a09b90c9e70d12ea3995783bb69ef6f62a51
alanna <alanna.june.moss@gmail.com>
2023-05-26 13:27:06 -0400

    Added temporary create endpoint and unit tests wit populated test database.

--- vindecode/main.py
+++ vindecode/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
 from vindecode import crud, models, schemas
 from vindecode.database import SessionLocal, engine
@@ -21,9 +22,20 @@ async def root():
     return {"message": "Welcome to the Vin Decode Application!"}
 
 
-@app.get("/lookup/{vin}")
-async def lookup(vin: str):
-    return {"vin": vin}
+@app.get("/lookup/{vin}", response_model=schemas.DecodedVIN)
+async def lookup(vin: str, db: Session = Depends(get_db)):
+    db_vin = crud.get_decoded_vin(db, vin=vin)
+    if db_vin is None:
+        raise HTTPException(status_code=404, detail="VIN not found")
+    return db_vin
+
+
+@app.post("/vins/", response_model=schemas.DecodedVIN)
+async def create_vin(vin: schemas.DecodedVINCreate, db: Session = Depends(get_db)):
+    db_vin = crud.get_decoded_vin(db, vin=vin.vin)
+    if db_vin:
+        raise HTTPException(status_code=400, detail="VIN already added.")
+    return crud.create_decoded_vin(db, vin=vin)
 
 
 @app.get("/remove/{vin}")
--- vindecode/tests/test_vindecode.py
+++ vindecode/tests/test_vindecode.py
@@ -3,7 +3,9 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from vindecode.crud import create_decoded_vin
 from vindecode.database import Base
+from vindecode.schemas import DecodedVINCreate
 from vindecode.main import app, get_db
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
@@ -22,11 +24,23 @@ def override_get_db():
         db.close()
 
 
-@pytest.fixture()
+@pytest.fixture
 def test_db():
     Base.metadata.create_all(bind=engine)
     yield
     Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture
+def test_db_vins(test_db):
+    db_vin = DecodedVINCreate(
+        vin="1XPWD40X1ED215307",
+        make="PETERBILT",
+        model="388",
+        model_year="2014",
+        body_class="Truck-Tractor"
+    )
+    create_decoded_vin(test_db, db_vin)
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -34,12 +48,56 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 
-def test_lookup():
-    response = client.get("/lookup/1XPWD40X1ED215307")
+def test_lookup_success(test_db_vins, test_db):
+    vin = "1XPWD40X1ED215307"
+    response = client.get("/lookup/{}".format(vin))
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["vin"] == "1XPWD40X1ED215307"
+    assert data["make"] == "PETERBILT"
+    assert data["model"] == "388"
+    assert data["model_year"] == "2014"
+    assert data["body_class"] == "Truck-Tractor"
+
+
+def test_lookup_not_found(test_db):
+    vin = "1XKWDB0X57J211825"
+    response = client.get("/lookup/{}".format(vin))
+    assert response.status_code == 404
+
+
+def test_create_new_vin_success(test_db):
+    vin_data = {
+            "vin": "1XKWDB0X57J211825",
+            "make": "KENWORTH",
+            "model": "W9 Series",
+            "model_year": "2007",
+            "body_class": "Truck-Tractor"
+        }
+    response = client.post(
+        "/vins/",
+        json=vin_data
+    )
     assert response.status_code == 200
-    assert response.json() == {
-        "vin": "1XPWD40X1ED215307"
-    }
+    data = response.json()
+    assert data == vin_data
+
+
+def test_create_existing_vin_success(test_db):
+    vin_data = {
+            "vin": "1XPWD40X1ED215307",
+            "make": "PETERBILT",
+            "model": "388",
+            "model_year": "2014",
+            "body_class": "Truck-Tractor"
+        }
+    response = client.post(
+        "/vins/",
+        json=vin_data
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data == vin_data
 
 
 def test_remove():
